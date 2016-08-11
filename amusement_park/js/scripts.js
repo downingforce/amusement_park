32cc66ed21fe42bc5200032af2d116e8a2cb33c9
Ben Vissotzky and Jason Downing <jason@>
2016-08-11 16:29:01 -0700

    Added second survey, added if functionality, added counter, added if to read counter and assign roller coaster

--- amusement_park/js/scripts.js
+++ amusement_park/js/scripts.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
   var userHeight = null;
+  var tea = 1;
+  var fantasy = 2;
+  var rocket = 3;
+  var batman = 4;
+  var terror = 5;
+  var melter = 6;
+  var wrong = 8;
+
+  var counter = 0;
+
+
 
   $(".survey_results form").submit(function(event) {
     var height = parseInt(($("#feet").val() * 12)) + parseInt($("#inches").val());
@@ -13,21 +24,62 @@ $(document).ready(function(){
 
     if (userHeight === "small") {
       $(".small_fry").show();
-      $("#small").show();
     } else if (userHeight === "normal") {
       $(".small_fry").show();
-      $("#small").show();
       $(".normal_fry").show();
-      $("#normal").show();
     } else {
       $(".small_fry").show();
-      $("#small").show();
       $(".normal_fry").show();
-      $("#normal").show();
       $(".big_fry").show();
-      $("#big").show();
+    }
+    $("#second_jumbotron").show();
+    event.preventDefault();
+  });
+  $("#survey2").submit(function(event) {
+    if($('#rail').is(':checked')) {
+      counter += 1;
+    }
+    if($('#water').is(':checked')) {
+      counter += 1;
+    }
+    if($('#drops').is(':checked')) {
+      counter += 1;
+    }
+    if($('#loops').is(':checked')) {
+      counter += 1;
+    }
+    if($('#slow').is(':checked')) {
+      counter += 1;
+    }
+    if($('#medium').is(':checked')) {
+      counter += 1;
+    }
+    if($('#fast').is(':checked')) {
+      counter += 1;
+    }
+    if (counter === 1) {
+      $(".tea2").show();
+    }
+    if (counter === 2) {
+      $(".fantasy2").show();
+    }
+    if (counter === 3) {
+      $(".rocket2").show();
+    }
+    if (counter === 4) {
+      $(".batman2").show();
+    }
+    if (counter === 5) {
+      $(".terror2").show();
+    }
+    if (counter === 6) {
+      $(".melter2").show();
+    }
+    if (counter >= 7) {
+      $(".wrong2").show();
     }
 
     event.preventDefault();
   });
+
 });
